dbfe3b8836bac354016f2f22783bcc53095c776a
Eric Rasche <rasche.eric@gmail.com>
2017-02-05 04:10:07 +0000

    extra workspace

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -113,8 +113,8 @@ local layouts = {
 
 -- {{{ Tags
 tags = {
-   names = { "Net", "Dev", "Net", "Sec"},
-   layout = { layouts[1], layouts[1], layouts[1], layouts[1] }
+   names = { "Net", "Dev", "Net", "Com", "Sec"},
+   layout = { layouts[1], layouts[1], layouts[1], layouts[1], layouts[1] }
 }
 for s = 1, screen.count() do
    tags[s] = awful.tag(tags.names, s, tags.layout)
